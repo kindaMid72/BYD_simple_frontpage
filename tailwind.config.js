26c465ab2d2eb390ffe030d571ca5983e7051fa0
King Ahmad <kingahmadilyas03@gmail.comdn>
2025-07-29 13:49:50 +0800

    fix the tailwind config issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*.{html,js}','./dist/*.{html,js}'],
+  content: ['./src/*.html', './dist/*.js', './src/*.js'],
   theme: {
     extend: {},
   },
